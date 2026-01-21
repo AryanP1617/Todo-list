10ed031640cee7f82714eb4faa137c0c90128702
AryanP1617 <punalekarar@gmail.com>
2026-01-21 20:11:44 +0530

    lets see

--- frontend/src/utils/refresh.js
+++ frontend/src/utils/refresh.js
@@ -5,6 +5,6 @@ export const refreshAccessToken=async ()=>{
   try {
     const response=await axios.post("https://todo-list-7226.onrender.com/api/users/refresh-token")
   } catch (error) {
-    window.location.href='/login'
+    
   }
 }
