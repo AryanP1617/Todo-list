543875daa78b998b3d385c7408f789e34da8601f
AryanP1617 <punalekarar@gmail.com>
2026-01-21 20:36:21 +0530

    Revert "Change 3"
    This reverts commit 392d4ffc4aadc9429fa27eb62901e2b3957aee81.

--- frontend/src/utils/refresh.js
+++ frontend/src/utils/refresh.js
@@ -8,23 +8,19 @@ const api = axios.create({
 api.interceptors.response.use(
   (response) => response,
   async (error) => {
-    
     const originalRequest = error.config;
 
     if (error.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
 
       try {
-        console.log("The token has expired !!!")
         await axios.post('/api/users/refresh-token', {}, { 
           withCredentials: true 
         });
 
         return api(originalRequest);
       } catch (refreshError) {
-
-        // window.location.href = '/login';
-        console.log("The refreshToken has somehow expired this is the error",refreshError)
+        window.location.href = '/login';
         return Promise.reject(refreshError);
       }
     }
