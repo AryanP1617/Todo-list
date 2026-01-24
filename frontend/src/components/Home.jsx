f7d4617212856bcbea7fda8974116b0d70b06a55
AryanP1617 <punalekarar@gmail.com>
2026-01-24 18:22:40 +0530

    Added logout function

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,24 @@
 import TaskList from "./TaskList.jsx"
 import './Home.css'
+import axiosInstance from "../utils/refresh.js"
+import { useNavigate } from "react-router-dom"
 
 function Home(){
 
+    const navigate=useNavigate()
+    const logOut=async()=>{
+        const response=await axiosInstance.post('/users/logout')
+        if(response.data.success===true)
+            navigate('/')
+    }   
     return(
         <>
-            <div id="body">
-                <TaskList/>
+            <div id="wrapper">
+                <div id="features">
+                    <button id="logoutButton" onClick={()=>logOut}>Logout</button>
+                    <TaskList/>                     
+                </div>
+                
             </div>            
         </>
     )    
